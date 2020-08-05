9ae9555355e3289662d592db954e16ea48b0bbea
Abhishek <M1050754@mindtree.com>
2020-08-05 22:32:14 +0530

    added seperate function for getting payload from token

--- utils/jwt.js
+++ utils/jwt.js
@@ -8,23 +8,31 @@ const verifyToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader)
+    // if header not present
     return res.status(401).json({ message: 'Unauthorized request' });
 
   const token = authHeader.split(' ')[1];
 
+  // if token is empty
   if (!token) return res.status(401).json({ message: 'Unauthorized request' });
 
+  const data = getPayloadFromToken(token);
+  if (data === null)
+    return res.status(401).json({ message: 'Unauthorized request' });
+
+  req.userId = data.subject;
+  next();
+};
+
+const getPayloadFromToken = (token) => {
   try {
-    const data = jwt.verify(token, process.env.SECRET_KEY);
-    req.userId = data.subject;
-    next();
+    return jwt.verify(token, process.env.SECRET_KEY);
   } catch (error) {
     console.log(error);
-    return res.status(401).json({ message: 'Unauthorized request' });
+    return null;
   }
 };
 
-const get
-
 module.exports.generateToken = generateToken;
 module.exports.verifyToken = verifyToken;
+module.exports.getPayloadFromToken = getPayloadFromToken;
